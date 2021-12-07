72c83464746969c35154f832443a4aeb8b9d7e54
SkinnDeee <southwickjt@yahoo.com>
2021-12-06 17:23:47 -0700

    init

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+let pokemon;
+
+function setActivePokemon(rawData) {
+  pokemon = rawData;
+  console.log(pokemon)
+
+  let display = document.getElementById("foundPokemon");
+  let title = `A wild ${pokemon.name} has appeared!`;
+  let img = pokemon.sprites.front_default;
+
+  display.innerHTML = `
+  <div>
+  ${title}
+  </div>
+  <img src="${img}" alt="error loading image" >
+  `
+
+}
+
+function getFromApi(id) {
+  fetch("https://pokeapi.co/api/v2/pokemon/151", {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  })
+    .then(res => res.json())
+    .then(res => {
+      setActivePokemon(res)
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+function findRandomPokemon() {
+  let id = Math.floor(Math.random() * 151);
+
+  getFromApi(id);
+}
